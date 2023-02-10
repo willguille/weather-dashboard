e7513d03d922c9492d6f664557ca4781e1e0c0fa
wilguille <willyguille97@gmail.com>
2023-02-10 19:59:53 +0000

    Dinamically added city title and date for today

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var historyDiv = $("#history");
 var todayDiv = $("#today");
 var forecastDiv = $("#forecast");
+var now = moment().format("DD/MM/YYYY");
 
 var APIUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
 var APIKey = "&appid=32306d8e68f68c9295a794f157aaab66";
@@ -9,11 +10,13 @@ var APIKey = "&appid=32306d8e68f68c9295a794f157aaab66";
 $("#search-button").on("click", function (event) {
     event.preventDefault();
     var city = $("#search-input").val();
-
     $.ajax({
         url: APIUrl + city + APIKey,
         success: function (data) {
             console.log(data);
+
+            var todayTitle = $("<h2>").text(data.city.name + " (" + now + ") ");
+            todayDiv.append(todayTitle);
         }
     })
 
