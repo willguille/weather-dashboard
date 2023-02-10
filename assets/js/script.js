2e0494615c915ec209917c8cfd1a431dccb489a7
wilguille <willyguille97@gmail.com>
2023-02-10 19:29:25 +0000

    Saved divs into variables and created event listener on search button that makes an ajax call

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Create variables to save divs
+var historyDiv = $("#history");
+var todayDiv = $("#today");
+var forecastDiv = $("#forecast");
+
+var APIUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
+var APIKey = "&appid=32306d8e68f68c9295a794f157aaab66";
+
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    var city = $("#search-input").val();
+
+    $.ajax({
+        url: APIUrl + city + APIKey,
+        success: function (data) {
+            console.log(data);
+        }
+    })
+
+}
+)
